5493fffd9a788df18cf6c1ab11ed6a93d391ec57
aleksiy325 <aleksiy123@gmail.com>
2018-01-13 01:13:04 -0800

    all commands added, need to think about error and organization

--- workload_generator/commands/commands.go
+++ workload_generator/commands/commands.go
@@ -6,39 +6,161 @@ import (
 )
 
 type Command struct {
-    Name string
-    Username string
-    Symbol string
-    Amount float64
+    Name string `json:",omitempty"`
+    Username string `json:",omitempty"`
+    Symbol string `json:",omitempty"`
+    Amount float64 `json:",omitempty"`
+    Filename string `json:",omitempty"`
 }
 
-func ParseAdd(data []string) Command {
-    amount, err := strconv.ParseFloat(data[3], 64)
+func parseCommandUserSymbolAmount(data []string) Command {
+	amount, err := strconv.ParseFloat(data[4], 64)
     if err != nil{
-        log.Fatal("Could not parse Amount")
+        log.Fatalf("Could not parse Amount: %s \n %s" , data[4], data)
+    }
+    return Command{Name: data[1], Username: data[2], Symbol: data[3], Amount: amount}
+}
+
+func parseCommandUserAmount(data []string) Command {
+	amount, err := strconv.ParseFloat(data[3], 64)
+    if err != nil{
+        log.Fatalf("Could not parse Amount: %s \n %s" , data[3], data)
     }
     return Command{Name: data[1], Username: data[2], Amount: amount}
 }
 
-
-func ParseQuote(data []string) Command {
+func parseCommandUserSymbol(data []string) Command {
     return Command{Name: data[1], Username: data[2], Symbol: data[3]}
 }
+ 
+func parseCommandUser(data []string) Command {
+    return Command{Name: data[1], Username: data[2]}
+}
 
+func ParseAdd(data []string) Command {
+   	return parseCommandUserAmount(data)
+}
+
+func ParseQuote(data []string) Command {
+    return parseCommandUserSymbol(data)
+}
 
 func ParseBuy(data []string) Command {
-    amount, err := strconv.ParseFloat(data[4], 64)
-    if err != nil{
-        log.Fatal("Could not parse Amount")
-    }
-    return Command{Name: data[1], Username: data[2], Symbol: data[3], Amount: amount}
+   	return parseCommandUserSymbolAmount(data)
+}
+
+func ParseSell(data []string) Command {
+   	return parseCommandUserSymbolAmount(data)
+}
+
+func ParseCommitBuy(data []string) Command {
+  	return parseCommandUser(data)
+}
+
+func ParseCommitSell(data []string) Command {
+   	return parseCommandUser(data)
+}
+
+func ParseCancelSell(data []string) Command {
+	return parseCommandUser(data)
+}
+
+func ParseCancelBuy(data []string) Command {
+	return parseCommandUser(data)
+}
+
+func ParseDisplaySummary(data []string) Command {
+	return parseCommandUser(data)
+}
+
+func ParseSetSellTrigger(data []string) Command {
+   	return parseCommandUserSymbolAmount(data)
+}
+
+func ParseSetBuyTrigger(data []string) Command {
+   	return parseCommandUserSymbolAmount(data)
+}
+
+func ParseCancelSetSell(data []string) Command {
+    return parseCommandUserSymbol(data)
+}
+
+func ParseCancelSetBuy(data []string) Command {
+    return parseCommandUserSymbol(data)
 }
 
 
-func ParseSell(data []string) Command {
-    amount, err := strconv.ParseFloat(data[4], 64)
-    if err != nil{
-        log.Fatal("Could not parse Amount")
+func ParseSetSellAmount(data []string) Command {
+    return parseCommandUserSymbolAmount(data)
+}
+
+func ParseSetBuyAmount(data []string) Command {
+    return parseCommandUserSymbolAmount(data)
+}
+
+func ParseDumplog(data []string) Command {
+	return Command{Name: data[1], Filename: data[2]}
+}
+
+func ParseData(data []string) Command {
+    if len(data) < 2{
+        log.Fatal("Length of entry too short")
     }
-    return Command{Name: data[1], Username: data[2], Symbol: data[3], Amount: amount}
+    var command Command
+
+    switch cmdName := data[1]; cmdName {
+        case "ADD":
+            command = ParseAdd(data)
+
+        case "QUOTE":
+            command = ParseQuote(data)
+
+        case "BUY":
+            command = ParseBuy(data)
+
+        case "SELL":
+            command = ParseSell(data)
+
+        case "COMMIT_BUY":
+            command = ParseCommitBuy(data)
+
+        case "COMMIT_SELL":
+            command = ParseCommitSell(data)
+
+        case "CANCEL_BUY":
+            command = ParseCommitBuy(data)
+
+        case "CANCEL_SELL":
+            command = ParseCommitSell(data)
+
+        case "DISPLAY_SUMMARY":
+            command = ParseCommitSell(data)
+
+        case "SET_BUY_TRIGGER":
+            command = ParseSetBuyTrigger(data)
+
+        case "SET_SELL_TRIGGER":
+            command = ParseSetSellTrigger(data)
+
+        case "CANCEL_SET_BUY":
+            command = ParseCancelSetBuy(data)
+
+        case "CANCEL_SET_SELL":
+            command = ParseCancelSetSell(data)
+
+        case "SET_BUY_AMOUNT":
+            command = ParseCancelSetBuy(data)
+
+        case "SET_SELL_AMOUNT":
+            command = ParseCancelSetSell(data)
+
+        case "DUMPLOG":
+            command = ParseDumplog(data)
+
+        default:
+            log.Fatalf("Invalid command: %s", data[1])
+    }
+
+    log.Println(command)
+    return command
 }
--- workload_generator/generator.go
+++ workload_generator/generator.go
@@ -1,42 +1,13 @@
 package main
 
 import (
+    "encoding/json"
     "bufio"
     "strings"
-    "fmt"
     "log"
     "os"
     "./commands"
 )
-
-
-func ParseData(data []string) commands.Command {
-    if len(data) < 2{
-        log.Fatal("Length of entry too short")
-    }
-    var command commands.Command
-
-    switch cmdName := data[1]; cmdName {
-        case "ADD":
-            command = commands.ParseAdd(data)
-
-        case "QUOTE":
-            command = commands.ParseQuote(data)
-
-        case "BUY":
-            command = commands.ParseBuy(data)
-
-        case "SELL":
-            command = commands.ParseSell(data)
-
-
-        default:
-            log.Fatal("Invalid command")
-    }
-
-    fmt.Println(command)
-    return command
-}
 
 func main() {
     file, err := os.Open("workfiles/1userWorkLoad")
@@ -52,8 +23,13 @@ func main() {
     for scanner.Scan() {
         line := replacer.Replace(scanner.Text())
         data := strings.Fields(line)
-        command := ParseData(data)
+        command := commands.ParseData(data)
         cmds = append(cmds, command)
+        js, err := json.Marshal(command)
+        if err != nil {
+            log.Fatal(err)
+        }
+        log.Printf("%s", js)
     }
 
     if err := scanner.Err(); err != nil {
