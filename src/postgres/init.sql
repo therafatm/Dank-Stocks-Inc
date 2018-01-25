961db9551e4a55614b7a21e03ecc317a77a5cab4
Cameron Long <cdalong@uvic.ca>
2018-01-25 13:25:23 -0800

    updated yaml and dockerfiles

---
+++ src/postgres/init.sql
@@ -0,0 +1,38 @@
+CREATE USER seng468 WITH PASSWORD 'SENG$^*';
+CREATE DATABASE transactions OWNER seng468;
+GRANT ALL PRIVILEGES ON DATABASE transactions TO seng468;
+
+\c transactions; 
+
+CREATE TABLE Users (
+    uid serial PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    money DOUBLE PRECISION NOT NULL
+);
+
+CREATE TABLE Stocks (
+    sid serial PRIMARY KEY,
+    username VARCHAR(50) REFERENCES Users(username),
+    symbol VARCHAR(10) NOT NULL,
+    shares INTEGER NOT NULL
+);
+
+CREATE TABLE Reservations (
+    rid serial PRIMARY KEY,
+    username VARCHAR(50) REFERENCES Users(username),
+    symbol VARCHAR(10),
+    type VARCHAR(10),
+    shares INTEGER NOT NULL,
+    amount DOUBLE PRECISION NOT NULL,
+    time BIGINT NOT NULL
+);
+
+CREATE TABLE Triggers (
+    tid serial PRIMARY KEY,
+    username VARCHAR(50) REFERENCES Users(username),
+    symbol VARCHAR(10) NOT NULL,
+    type VARCHAR(10) NOT NULL,
+    amount DOUBLE PRECISION,
+    shares INTEGER,
+    trigger_price DOUBLE PRECISION
+);
